5228fe1574e5b56a8a83f6720758da35a7d3cd98
DankraniKun <nicopla808@gmail.com>
2025-04-08 12:26:14 +0200

    Inclusión del proyecto como tal?

---
+++ src/main/java/com/mycompany/_proyfinaldi_npmolla/_ProyFinalDI_NPMolla.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._proyfinaldi_npmolla;
+
+/**
+ *
+ * @author nicop
+ */
+public class _ProyFinalDI_NPMolla {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
